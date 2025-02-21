be6b6b1876b48ce6771f317a2d0c1ec2cf5f8f07
Matteo <matteo.grisenti@studenti.unitn.it>
2025-02-21 09:39:44 +0100

    Setup

---
+++ custom-admin-user-page.php
@@ -0,0 +1,10 @@
+<php? 
+/*
+* Plugin Name:       Custom Admin User Page
+* Description:       Custom plugin to menage acess of admin entity of a website.
+* Version:           0.0.1
+* Author:            Matteo Grisenti
+* License:           GPL v2 or later
+* Requires Plugins:  roller-foto
+*/
+ 
